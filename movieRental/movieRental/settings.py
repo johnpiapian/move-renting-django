92a127d706971acdbea0d9c8db0a3b1ce66c49c2
John (Sut) <johnpiapian@gmail.com>
2023-06-10 00:06:10 -0500

    Fix: allow all hosts (#2)

--- movieRental/movieRental/settings.py
+++ movieRental/movieRental/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9#pn+_t&3^!)bo=mg)5-j9m-dkiuys%8vt06xnf-b*y@g56y4l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
