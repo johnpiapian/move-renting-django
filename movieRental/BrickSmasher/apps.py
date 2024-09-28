45aca2933a2148f9e56d031f4f6afa8fcb963656
johnpiapian <johnpiapian@gmail.com>
2024-09-27 21:47:03 -0500

    feat: init database migration on start

--- movieRental/BrickSmasher/apps.py
+++ movieRental/BrickSmasher/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
+from django.core.management import call_command
 
 
 class BricksmasherConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'BrickSmasher'
+
+    def ready(self) -> None:
+        call_command('migrate')
